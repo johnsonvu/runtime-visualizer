573bd7a25113438234427c506e29b2c99d5bc1e4
Johnson Vu <johnsonvu96@gmail.com>
2019-11-18 16:56:47 -0800

    Add visualization and example data format to backend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,46 @@
 var fs = require("fs");
 var express = require("express");
+var cors = require("cors");
+
 const app = express();
+app.use(cors());
 
 var extract = require("extract-zip");
 
 
 app.get('/',function(req,res){
     res.send("Hello Johnson!");
+});
+
+// This is an example of how the data should be formatted
+app.get('/example',function(req,res){
+    const data = {
+        "nodes": [
+            {
+            "id": "1",
+            "name": "node1",
+            "color": "red",
+            "val": 20
+            },
+            {
+            "id": "2",
+            "name": "node2",
+            "color": "green",
+            "val": 10
+            }
+        ],
+        "links": [
+            {
+            "source": "1",
+            "target": "2",
+            "width": 5,
+            "color": "#999",
+            "distance": 60
+            }
+        ]
+    };
+
+    res.send(JSON.stringify(data));
 });
 
 app.listen(3001, () => {})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ForceGraph2D} from 'react-force-graph';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.graphRef = React.createRef();
+
+    this.state = {
+      data: {
+        nodes: [],
+        links: []
+      },
+    }
+  }
+
+  requestData() {
+    axios.get(`http://localhost:3001/example`)
+      .then(res => {
+        const data = res.data;
+        console.log(data);
+        this.setState({data: data});
+    });
+  }
+
+  componentDidMount() {
+    this.requestData();
+    // applies the distance to the links dynamically after mounting
+    const graph = this.graphRef.current;
+    graph.d3Force('link').distance(link => link.distance).iterations(30);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ForceGraph2D 
+          ref={this.graphRef}
+          graphData={this.state.data} 
+          linkWidth="width"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
