654c628c19b69e102eb05599cc59c4f884522fea
Johnson Vu <johnsonvu96@gmail.com>
2019-11-18 17:10:55 -0800

    Update example endpoint

--- backend/app.js
+++ backend/app.js
@@ -17,25 +17,38 @@ app.get('/example',function(req,res){
     const data = {
         "nodes": [
             {
-            "id": "1",
-            "name": "node1",
-            "color": "red",
-            "val": 20
+                "id": "1",
+                "name": "node1",
+                "color": "red",
+                "val": 20
             },
             {
-            "id": "2",
-            "name": "node2",
-            "color": "green",
-            "val": 10
+                "id": "2",
+                "name": "node2",
+                "color": "green",
+                "val": 10
+            },
+            {
+                "id": "3",
+                "name": "node3",
+                "color": "blue",
+                "val": 50
             }
         ],
         "links": [
             {
-            "source": "1",
-            "target": "2",
-            "width": 5,
-            "color": "#999",
-            "distance": 60
+                "source": "1",
+                "target": "2",
+                "width": 5,
+                "color": "#999",
+                "distance": 60
+            },
+            {
+                "source": "3",
+                "target": "2",
+                "width": 1,
+                "color": "#999",
+                "distance": 100
             }
         ]
     };
