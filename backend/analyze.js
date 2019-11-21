5d20cedf587aad14c5a2003b48c6ee742d14f932
Justin Kwan <justinkkwan@alumni.ubc.ca>
2019-11-20 17:59:00 -0800

    fixed leon's stuff

--- backend/analyze.js
+++ backend/analyze.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const injectPythonCode = () => {
     let pythonFiles = findPythonFiles(__dirname + "/analyze");
     console.log(JSON.stringify(pythonFiles));
-    readFile();
+    pythonFiles.forEach(injectRefelctions);
 }
 
 function findPythonFiles(directory){
@@ -23,14 +23,9 @@ function findPythonFiles(directory){
     return pythonFiles;
 }
 
-function readFile(){
-    var input = 'C:\\Users\\Leonl\\OneDrive\\Documents\\cpsc 410\\runtime-visualizer\\exampleProjects\\LibraryBook.py';
-
-    fs.readFile(input, 'utf8', function (err, data) {
-        if(err)
-            return console.log(err);
-        console.log('Result: ' + data);
-    });
+function injectRefelctions(filePath){
+    let contents = fs.readFileSync(filePath, "utf8");
+    console.log(contents);
 }
   
   
