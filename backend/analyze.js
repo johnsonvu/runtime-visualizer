95b8c45d6f8c52a17643feeeb24d111a8ececfee
Justin Kwan <justinkkwan@alumni.ubc.ca>
2019-11-23 21:52:14 -0800

    Changing backend structure

--- backend/analyze.js
+++ backend/analyze.js
@@ -1,17 +1,19 @@
-const fs = require("fs");
+const fs = require('fs');
+const inputAnalyzer = require('./analyze/inputAnalysis.js');
+const runtimeAnalyzer = require('./analyze/runtimeAnalysis.js');
 
 const injectPythonCode = () => {
-    let pythonFiles = findPythonFiles(__dirname + "/analyze");
-    console.log(JSON.stringify(pythonFiles));
-    pythonFiles.forEach(injectRefelctions);
+    let pythonFiles = findPythonFiles(__dirname + '/analyze');
+    let inputInfo = inputAnalyzer.getInputInfo(pythonFiles);
+    runtimeAnalyzer.analyzeRuntime(pythonFiles, inputInfo);
 }
 
 function findPythonFiles(directory){
     let pythonFiles = [];
     fs.readdirSync(directory)
         .forEach((fileName) => {
-            let fullFilePath = directory + "/" + fileName
-            if(fullFilePath.endsWith(".py")){
+            let fullFilePath = directory + '/' + fileName
+            if(fullFilePath.endsWith('.py')){
                 pythonFiles.push(fullFilePath);
             }
             else if(fs.statSync(fullFilePath).isDirectory()){
@@ -23,43 +25,4 @@ function findPythonFiles(directory){
     return pythonFiles;
 }
 
-function injectRefelctions(filePath){
-    let contents = fs.readFileSync(filePath, 'utf8').split('\n').map((line)=>line.replace(/    /g, '\t'));
-    let modifiedContent = injectReflectionCode(contents);
-    fs.writeFileSync(filePath, modifiedContent);
-}
-
-function numOfTabs(line){
-    let i = 0;
-    let count = 0;
-    while(line.charAt(i++) === '\t'){
-      count++;
-    }
-
-    return count;
-}
-
-function injectReflectionCode(fileContent){
-    let modifiedContent = [];
-    modifiedContent.push('from memory_profiler import profile\r');
-    modifiedContent.push('import inspect\r');
-    modifiedContent.push('from codeAnalyzer import codeAnalyzer\r');
-    for(let i = 0; i <fileContent.length; i++ ){
-        let line = fileContent[i];
-        modifiedContent.push(line);
-        if(line.trim().startsWith('def')){
-            let numTabs = numOfTabs(line);
-            modifiedContent.push('\t'.repeat(numTabs + 1) + 'functionName = inspect.stack()[0][3]\r');
-            // modifiedContent.push('\t'.repeat(numTabs + 1) + 'print "{} --> {}".format(inspect.stack()[1][3], functionName)\r');
-        }
-    }
-    //console.log(modifiedContent);
-    let stringContent = "";
-    for(let i = 0; i <modifiedContent.length; i++ ){
-        stringContent = stringContent.concat(modifiedContent[i]);
-    }
-    return stringContent
-}
-
-  
 module.exports = { injectPythonCode: injectPythonCode };
