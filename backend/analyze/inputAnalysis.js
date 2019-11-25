1e6282ede98896ed4796d6fe12d952fd8b4a1570
Justin Kwan <justinkkwan@alumni.ubc.ca>
2019-11-24 19:04:16 -0800

    Added some stuff, not tested

--- backend/analyze/inputAnalysis.js
+++ backend/analyze/inputAnalysis.js
@@ -1,13 +1,59 @@
 const fs = require('fs');
+var currentWorkingFunction = null;
+var dictionary = {};
 
 function getInputInfo(pythonFiles){
-    pythonFiles.map(pythonFile => extractInfo(pythonFile));
-    return null;
+    let info = pythonFiles.map(pythonFile => { return { file: pythonFile, info: extractInfo(pythonFile) }; })};
+    return info;
 }
 
 function extractInfo(pythonFile){
     console.log('Analyzing inputs in ' + pythonFile + '\n');
-    let lines = fs.readFileSync(pythonFile, 'utf8').split('\n').map((line)=>line.replace(/    /g, '\t'));
+    let content = fs.readFileSync(pythonFile, 'utf8').replace(/\n    /g, '\n\t');
+    let lines = content.split('\n').map((line)=>line.replace(/    /g, '\t'));
+
+    let newFunctionRegExp = /[\t\n]def\s/;
+    let varDecRegExp = /[^\!\=]=[^=]/;
+    let functionCallRegExp = /[^\!\=]=[^=]/;
+    
+    for(let line of lines){
+        if(varDecRegExp.test(line)){
+            let varDec = line.split('=');
+            let varName = varDec[0].trim();
+            let rExpression = varDec[1].trim();
+            currentWorkingFunction[varName] = getDependencies(rExpression);
+        }
+        else if(newFunctionRegExp.test(line)){
+            let functionName = line.split('def')[1].split('(')[0].trim();
+            dictionary[functionName] = {};
+            currentWorkingFunction = dictionary[functionName];
+        }
+
+        if(line.indexOf('(') != -1){
+            let parts = line.split(/\(\,\)/);
+            
+            let functionName = parts[0].match(/[^\s=]+\s*$/).trim();
+
+            for (let i=1; i<parts.length-1; i++){
+                let input = part[i].trim();
+                getDefUse(input);
+            }
+        }
+    }
+}
+
+function getDependencies(rExpression){
+    let dependencies = [];
+    for (let variable in currentWorkingFunction){
+        if (rExpression.indexOf(variable) !== -1){
+            dependencies.push(variable);
+        }
+    }
+    return dependencies;
+}
+
+function getDefUse(variable){
+    currentWorkingFunction[variable];
 }
 
 module.exports = { getInputInfo: getInputInfo };
